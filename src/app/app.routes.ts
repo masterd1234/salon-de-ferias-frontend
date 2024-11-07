ea08b1f1efe41e212ce1e257597635c55cebffaf
Pedro <pedro.j.izdo@gmail.com>
2024-11-07 14:35:36 +0100

    ......

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,16 @@ import { AdminDashboardComponent } from './page/admin-dashboard/admin-dashboard.
 import { authGuard } from './guards/auth.guard';
 import { CompanyDashboarComponent } from './page/company-dashboar/company-dashboar.component';
 import { ProfileComponent } from './page/profile/profile.component';
-import { OffersComponent } from './page/profile/offers/offers.component';
+import { AllOffersComponent } from './page/all-offers/all-offers.component';
+
+
 
 
 export const routes: Routes = [
-    {path: '', component: LoginComponent},
-    {path: 'login', component: LoginComponent},
-    {path: 'admin-dashboard', component: AdminDashboardComponent, canActivate: [authGuard] },
-    {path: 'company-dashboard', component: CompanyDashboarComponent, canActivate: [authGuard] },
-    {path: 'profile', component: ProfileComponent, canActivate: [authGuard]},
-    {path: 'offers', component: OffersComponent, canActivate: [authGuard]}
+    { path: '', component: LoginComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'admin-dashboard', component: AdminDashboardComponent, canActivate: [authGuard] },
+    { path: 'company-dashboard', component: CompanyDashboarComponent, canActivate: [authGuard] },
+    { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
+    { path: 'offers', component: AllOffersComponent, canActivate: [authGuard] }
 ];
