fe75ff922bc84a4eb04f2b923aa6d253fcb5c560
Pedro <pedro.j.izdo@gmail.com>
2024-11-07 14:59:14 +0100

    build de añadir pagina inicio

--- dist/palacio-de-ferias/server/main.server.mjs
+++ dist/palacio-de-ferias/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-E52A64AN.mjs";import"./chunk-AVUJHHOR.mjs";import"./chunk-3CVZQN5Z.mjs";export{a as default};
+import{a}from"./chunk-2CFYINDC.mjs";import"./chunk-AVUJHHOR.mjs";import"./chunk-3CVZQN5Z.mjs";export{a as default};
