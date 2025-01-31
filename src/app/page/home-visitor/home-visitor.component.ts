8960931385899cd2a950eb32ee88ecdf6a3e1ad0
luisrivas123 <luis.rivas@correounivalle.edu.co>
2025-01-31 15:09:04 +0100

    add headear and video

--- src/app/page/home-visitor/home-visitor.component.ts
+++ src/app/page/home-visitor/home-visitor.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { GalleryCompanyComponent } from './gallery-company/gallery-company.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-home-visitor',
   standalone: true,
-  imports: [GalleryCompanyComponent],
+  imports: [GalleryCompanyComponent, HeaderComponent],
   templateUrl: './home-visitor.component.html',
   styleUrl: './home-visitor.component.scss',
 })
