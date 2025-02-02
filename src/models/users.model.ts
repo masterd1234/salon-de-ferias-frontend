ae85ea56fdf19be4ab36cc6c2e452c080a4ddddd
luisrivas123 <luis.rivas@correounivalle.edu.co>
2025-02-02 14:04:41 +0100

    update user model register

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -3,17 +3,18 @@
  * @description Interfaz que representa los datos de un usuario en la aplicación, incluyendo propiedades opcionales específicas para diferentes roles (admin, co y visitor).
  */
 export interface Usuario {
-id?: string;
-name: string;
-email: string;
-password: string;
-rol: string;
-file?: File;
-cv?: File;
-phone?: number;
-subname?: string;
-studies?: string;
-cif?: string;
-dni?: string;
-logo?: string;
+  id?: string;
+  name: string;
+  email: string;
+  password: string;
+  rol: string;
+  file?: File;
+  cv?: File;
+  phone?: number;
+  subname?: string;
+  studies?: string;
+  cif?: string;
+  dni?: string;
+  logo?: string;
+  image?: string;
 }
